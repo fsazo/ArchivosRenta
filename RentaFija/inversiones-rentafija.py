2aec54a0b4b6c17ad9a82e50433e3698b7b70e45
Francisca Sazo <fsazo@rentanacional.cl>
2025-10-27 16:52:18 -0300

    cambios

--- RentaFija/inversiones-rentafija.py
+++ RentaFija/inversiones-rentafija.py
@@ -951,7 +951,7 @@ if not df.empty:
     
     # Ajustes visuales
     fig_treemap.update_traces(
-        hovertemplate="<b>%{label}</b><br>Valor Final: %{value:,.0f} UF<extra></extra>",
+        hovertemplate='<b>%{label}</b><br>Valor Final: %{value:,.0f} UF<extra></extra>',
     
     )
 
